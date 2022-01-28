d4f94cd9c8a00828bbd4ccbbd2021ae697bba81b
Andrey Ryabov <andrey.ryabov@ra-company.kz>
2022-01-28 12:52:35 +0600

    Library was created

---
+++ lib/anoubis_sso_server.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "anoubis_sso_server/version"
+
+module AnoubisSsoServer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/anoubis_sso_server_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AnoubisSsoServer do
+  it "has a version number" do
+    expect(AnoubisSsoServer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
